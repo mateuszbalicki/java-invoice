e160c4e5e1fc3b954ad42d699bd3f21bef843a1f
mbalicki <mbalicki@d17-426b-pc-12.ipa.ii.agh.edu.pl>
2026-01-11 13:55:27 +0100

    Second fix in Product class

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -9,10 +9,18 @@ public abstract class Product {
 
     private final BigDecimal taxPercent;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
-        this.name = name;
-        this.price = price;
-        this.taxPercent = tax;
+    protected Product(String name, BigDecimal price, BigDecimal tax) throws IllegalArgumentException {
+        if (name == null ||  price == null || tax == null) {
+            throw new IllegalArgumentException();
+        } else if (name.equals("") ||  price.equals("") || tax.equals("")) {
+            throw new IllegalArgumentException();
+        } else if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.name = name;
+            this.price = price;
+            this.taxPercent = tax;
+        }
     }
 
     public String getName() {
